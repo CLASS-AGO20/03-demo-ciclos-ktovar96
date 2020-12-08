220774211e54a457c4a614fc027306f6c14b76f2
Karla Tovar <ktovar@ucol.mx>
2020-12-07 23:07:05 -0600

    Implemented sumaarParesWhile

--- src/main.js
+++ src/main.js
@@ -25,8 +25,22 @@ export default class App {
         }
         return impares;
     }
+
+    sumarParesWhile() {
+        let i= 1;
+        let suma = 0;
+        while (i <= 20){
+        if (i %2 == 0){
+            suma =suma + i;
+        }
+        i = i + 1 ;
+        }
+        return suma;
+    }
+    
 }
 
 let app = new App();
 console.log (app.sumarParesFor());
 console.log (app.contarImparesFor(11, 5));
+console.log (app.sumarParesWhile());
